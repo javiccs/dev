7e9f8d14fbe1e6efe01cc80b2c75e281b8827d42
German Javier Perez Gonzalez <gperez10@gmail.com>
2017-11-03 11:35:53 -0400

    css for iframe

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,9 +2,10 @@ import {Component} from '@angular/core';
 import {IonicPage, NavController, AlertController, MenuController, LoadingController} from 'ionic-angular';
 import {MainPage} from '../main/main';
 import {Injectable} from '@angular/core';
-import { Http, ResponseContentType, Response, ResponseOptions } from '@angular/http';
+import {Http, ResponseContentType, Response, ResponseOptions} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import {AuthCognitoProvider} from '../../providers/auth-cognito.provider';
+
 /**
  * Generated class for the HomePage page.
  *
@@ -20,79 +21,81 @@ import {AuthCognitoProvider} from '../../providers/auth-cognito.provider';
 export class HomePage extends MainPage {
   requestURL: string;
   public items: any = [];
-  public landingPage:boolean;
+  public landingPage: boolean;
+
   constructor(public navCtrl: NavController,
               public loadingCtrl: LoadingController,
               public menu: MenuController,
               public http: Http,
               public awsProvider: AuthCognitoProvider,
               public alertCtrl: AlertController,) {
+
     super(navCtrl, loadingCtrl, menu, alertCtrl);
-
-    this.awsProvider.awsInit();
-    this.awsProvider.getCognitoUser(this)
-    this.presentLoading('Cargando home')
-    this.loading.present()
-    this.landingPage=true;
+    try {
+      console.log('entro a constructor')
+      this.awsProvider.awsInit();
+      this.awsProvider.getCognitoUser(this)
+      this.landingPage = true;
+    } catch (e) {
+      console.log('entro'+2)
+    }
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
-  }
-  ionViewWillEnter() {
+
+  doLogout() {
+try{
+  this.navCtrl.setRoot('LoginPage')
+  this.awsProvider.logout(this);
+}catch(e){
+  console.log(e)
+}
 
   }
 
-  doLogout(){
-
-    this.awsProvider.logout(this);
-  }
   isLoggedInCallback(message: string, isLoggedIn: boolean) {
-    //console.log('isLoggedInCallback main-page')
-    localStorage.clear();
-    this.navCtrl.popToRoot()
 
   }
+
   cognitoCallback(message: any, result: any) {
 
     if (message != null) { //error
-      this.loading.dismiss();
-      this.items={}
+      this.items = {}
     } else { //success
       try {
         var decodedString = String.fromCharCode.apply(null, new Uint8Array(result.Body));
-        this.items=JSON.parse(decodedString)
-        this.requestURL=this.items[0].src;
+        this.items = JSON.parse(decodedString)
+        this.requestURL = this.items[0].src;
         //console.log('items'+this.items)
         //this.items.push();
-        this.loading.dismiss()
-      }catch (e){
+      } catch (e) {
         console.log(e)
       }
     }
 
   }
-  getSource(item){
-    this.landingPage=false;
+
+  getSource(item) {
+    this.landingPage = false;
     this.presentLoading('Cargando')
     this.loading.present()
-    this.requestURL=item.src;
+    this.requestURL = item.src;
     this.loading.dismiss()
   }
+
   getUserCallback(message: any, result: any) {
-    let email:any;
+    let email: any;
     if (message != null) { //error
       console.log('error')
     } else { //success
       try {
-        for(var i = 0; i < result.length; i++) {
-          if(result[i].Name='email'){
-            email=result[i].Value;
+        for (var i = 0; i < result.length; i++) {
+          if (result[i].Name = 'email') {
+            email = result[i].Value;
           }
         }
-        let user=email.split("@");
-        this.awsProvider.getTableConfig(user[0],this)
-      }catch (e){
+        let user = email.split("@");
+        this.awsProvider.getTableConfig(user[0], this)
+      } catch (e) {
         console.log(e)
       }
     }
